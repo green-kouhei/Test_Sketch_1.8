b3bfcd262cd4ba90d7bbeeaf03cf8d83162bbe8b
green-kouhei <Kouhei@kouhei-no-MacBook-Pro.local>
2016-03-15 05:28:50 +0900

    つり目完成 ショートヘア完成 セーラー服途中

---
+++ src/sketch01/Test01.java
@@ -0,0 +1,24 @@
+package sketch01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Test01 {
+
+	// public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String line = br.readLine();
+		int n = Integer.parseInt(line);
+		try {
+			if (0 <= n | n<= 10) {
+				for (int i = 0; i < n; i++) {
+					System.out.print("Ann");
+				}
+			}
+		} catch (IndexOutOfBoundsException e) {
+			System.out.println("範囲外です");
+		}
+	}
+}
